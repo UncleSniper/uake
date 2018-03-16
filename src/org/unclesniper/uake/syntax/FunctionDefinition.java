c60ef75de32129f5ea5675d67ce43216ed187e2b
Simon Bausch <simon.bausch@cellent.de>
2018-03-16 15:29:56 +0100

    added syntax/FunctionDefinition

---
+++ src/org/unclesniper/uake/syntax/FunctionDefinition.java
@@ -0,0 +1,11 @@
+package org.unclesniper.uake.syntax;
+
+public class FunctionDefinition extends Definition {
+
+	private TypeSpecifier returnType;
+
+	private String name;
+
+	private Location nameLocation;
+
+}
